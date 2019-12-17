db76ade2fb29a3d50c7bd86904ae9a29c68a0cc8
okto prima <octoprima93@gmail.com>
2019-12-17 15:11:03 +0700

    Remove unused file

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/oktopriima/mark-ii/controller/ping"
 	"github.com/oktopriima/mark-ii/controller/users"
-	"net/http"
 )
 
 func main() {
@@ -15,20 +14,10 @@ func main() {
 	{
 		userRoute := r.Group("user")
 		userRoute.POST("", users.CreateController)
-		userRoute.PUT("", )
-		userRoute.GET("", )
-		userRoute.GET(":id", )
-		userRoute.DELETE(":id", )
-	}
-
-	{
-		orderRoute := r.Group("order")
-		orderRoute.GET("", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "OK",
-			})
-			return
-		})
+		//userRoute.PUT("", )
+		//userRoute.GET("", )
+		//userRoute.GET(":id", )
+		//userRoute.DELETE(":id", )
 	}
 
 	r.Run(":9000")
